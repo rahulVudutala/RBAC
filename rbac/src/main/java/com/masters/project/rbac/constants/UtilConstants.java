69f95d7946fb5ca1846d897ae8dff3bc14324b6f
rahulVudutala <rahulbharadwaj.v@gmail.com>
2017-12-01 17:12:44 -0800

    Commiting Util Constants

---
+++ rbac/src/main/java/com/masters/project/rbac/constants/UtilConstants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.masters.project.rbac.constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author rahul.vudutala
+ *
+ */
+public class UtilConstants {
+	// Database credentials
+	public static final String URL = "**";
+	public static final String USER = "**";
+	public static final String PASS = "**";
+	public static final String DB = "**";
+	
+	// Column Metadata
+	public static List<String> columnMetaData = new ArrayList<String>(); 
+}
