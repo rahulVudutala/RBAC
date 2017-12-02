2ec11d58d9a9362afe21d2157ca8d883c22a5057
PriyathamTadikonda <preethu4476@gmail.com>
2017-12-02 14:04:02 -0800

    Update TableController.java

--- rbac/src/main/java/com/masters/project/rbac/controller/TableController.java
+++ rbac/src/main/java/com/masters/project/rbac/controller/TableController.java
@@ -6,6 +6,7 @@ package com.masters.project.rbac.controller;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
